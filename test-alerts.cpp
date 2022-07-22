b8994c0cee8d81663d05978d9ae97f8151947a89
Deepa Bommineni (MS/EDX3-XC) <Bommineni.Deepa@in.bosch.com>
2022-07-22 17:06:10 +0530

    Test changed

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,38 +3,35 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-	REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-	REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
-	REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+TEST_CASE("Check the breach if its with in limits") {
+	REQUIRE(inferBreach(12, 20, 40) == TOO_LOW);
+	REQUIRE(inferBreach(50, 20, 40) == TOO_HIGH);
+	REQUIRE(inferBreach(25, 20, 40) == NORMAL);
 }
 
-TEST_CASE("classify Temperature breach with different value") {
-	BatteryCharacter batteryChar {PASSIVE_COOLING,"ELBattery"};
-	checkAndAlert(TO_CONTROLLER, batteryChar, 20) ;
-	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,20) == NORMAL);
-	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -20) == TOO_LOW);
+TEST_CASE("Divide and cover the temperature breach") {
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -5) == TOO_LOW);
 	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 10) == NORMAL);
 
-	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 20) == NORMAL);
-	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -20) == TOO_LOW);
+	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -5) == TOO_LOW);
 	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 50) == TOO_HIGH);
+	REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 10) == NORMAL);
 
-	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 20) == NORMAL);
-	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -20) == TOO_LOW);
+	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -5) == TOO_LOW);
 	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 50) == TOO_HIGH);
-
+	REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 10) == NORMAL);
 }
 
-TEST_CASE("Test Alert Target and construct message of To Controller") {
+TEST_CASE("Check alert and message to Controller") {
 	BatteryCharacter batteryChar {PASSIVE_COOLING,"ELBattery"};
-	checkAndAlert(TO_CONTROLLER, batteryChar, -20 ) ;
-	checkAndAlert(TO_CONTROLLER, batteryChar, 20) ;
+	checkAndAlert(TO_CONTROLLER, batteryChar, -5) ;
 	checkAndAlert(TO_CONTROLLER, batteryChar, 40) ;
+	checkAndAlert(TO_CONTROLLER, batteryChar, 10) ;
 }
-TEST_CASE("Test Alert Target and construct message of To Email") {
+TEST_CASE("Check Alert and message of To Email") {
 	BatteryCharacter batteryChar {PASSIVE_COOLING,"ELBattery"};
-	checkAndAlert(TO_EMAIL, batteryChar, -20 ) ;
-	checkAndAlert(TO_EMAIL, batteryChar, 20) ;
+	checkAndAlert(TO_EMAIL, batteryChar, -5) ;
 	checkAndAlert(TO_EMAIL, batteryChar, 40) ;
+	checkAndAlert(TO_EMAIL, batteryChar, 10) ;
 }
